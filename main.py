7212d3d4a5f1c767944ea3599d1f37bd74221340
Jun He <jhe@cs.wisc.edu>
2015-05-02 12:53:56 -0500

    implement mode 3

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import json
 import producer
 import random
-
-
+import numpy as np
 
 def load_json(fpath):
     decoded = json.load(open(fpath, 'r'))
@@ -60,7 +59,40 @@ def apply_single_model_02(conf, prod, fpath):
     for pair in pairs:
         print pair
         prod.addReadOrWrite2(op=optype, pid=0, path=fpath,
-                off=pairr["offset"], len=pair["size"])
+                off=pair["offset"], len=pair["size"])
+
+    prod.display()
+
+def apply_single_model_03(conf, prod, fpath):
+    if conf["mode"] != 3:
+        print 'wrong mode'
+        exit(1)
+
+    n_access = conf["n_access"]
+    optype = conf["type"]
+
+    size_mu = conf["sizedist"]["mu"]
+    size_sigma = conf["sizedist"]["sigma"]
+    sizes = get_lognorm_list(size_mu, size_sigma, n_access, 1000)
+
+    # sort the sizes
+    sizes.sort(key = lambda x: eval(conf["sizeorderhash"]))
+
+    # generate offsets
+    offsets = list(np.cumsum(sizes))
+    offsets.insert(0, 0)
+    offsets = offsets[0:-1]
+
+    # form offset, size pairs
+    pairs = [ {'offset':off, 'size':size} for off, size in zip(offsets, sizes)]
+
+    # sort pairs by offsetorder hash
+    pairs.sort(key = lambda x: eval(conf["offsetorderhash"]))
+
+    for pair in pairs:
+        print pair
+        prod.addReadOrWrite2(op=optype, pid=0, path=fpath,
+                off=pair["offset"], len=pair["size"])
 
     prod.display()
 
@@ -70,7 +102,8 @@ def main():
 
     global_conf = load_json('wl.json')
     # apply_single_model_01(global_conf["singles"]["single1"], prod)
-    apply_single_model_02(global_conf["singles"]["single2"], prod, 'mypath')
+    #apply_single_model_02(global_conf["singles"]["single2"], prod, 'mypath')
+    apply_single_model_03(global_conf["singles"]["single3"], prod, 'mypath')
 
 if __name__ == '__main__':
     main()
