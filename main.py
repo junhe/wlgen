b33dc30da9752921685df57c43f478fb31bc4a1e
Jun He <jhe@cs.wisc.edu>
2015-05-02 12:43:44 -0500

    implement mode 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import producer
+import random
 
 
 
@@ -22,6 +23,12 @@ def apply_single_model_01(conf, prod, fpath):
 
     prod.display()
 
+def get_lognorm_list(mu, sigma, n, factor):
+    l = [int(random.lognormvariate(mu, sigma)*factor)
+            for i in range(n)]
+    return l
+
+
 def apply_single_model_02(conf, prod, fpath):
     if conf["mode"] != 2:
         print 'wrong mode'
@@ -32,20 +39,38 @@ def apply_single_model_02(conf, prod, fpath):
 
     off_mu = conf["offsetdist"]["mu"]
     off_sigma = conf["offsetdist"]["sigma"]
-    offsets = [random.lognormvariate(off_mu, off_sigma) for i in range(n_access)]
-    print 'offsets', offsets
+    offsets = get_lognorm_list(off_mu, off_sigma, n_access, 1000)
 
     size_mu = conf["sizedist"]["mu"]
     size_sigma = conf["sizedist"]["sigma"]
-    sizes = [random.lognormvariate(size_mu, size_sigma) for i in range(n_access)]
-    print 'sizes', sizes
+    sizes = get_lognorm_list(size_mu, size_sigma, n_access, 1000)
+
+    # sort offset from small to large
+    offsets.sort()
+
+    # sort the sizes
+    sizes.sort(key = lambda x: eval(conf["sizeorderhash"]))
+
+    # form offset, size pairs
+    pairs = [ {'offset':off, 'size':size} for off, size in zip(offsets, sizes)]
+
+    # sort pairs by offsetorder hash
+    pairs.sort(key = lambda x: eval(conf["offsetorderhash"]))
+
+    for pair in pairs:
+        print pair
+        prod.addReadOrWrite2(op=optype, pid=0, path=fpath,
+                off=pairr["offset"], len=pair["size"])
+
+    prod.display()
 
 def main():
     prod = producer.Producer(rootdir="/tmp/",
             tofile="/tmp/producedfile.txt")
 
     global_conf = load_json('wl.json')
-    apply_single_model_01(global_conf["single"], prod)
+    # apply_single_model_01(global_conf["singles"]["single1"], prod)
+    apply_single_model_02(global_conf["singles"]["single2"], prod, 'mypath')
 
 if __name__ == '__main__':
     main()
