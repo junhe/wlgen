131b08fe6ccb92c893692c3a1e9f4f3ccfb169b9
Jun He <jhe@cs.wisc.edu>
2015-05-05 22:52:17 -0600

    add some files

---
+++ analyzer.r
@@ -0,0 +1,52 @@
+# libraries
+# library(ggplot2)
+# library(plyr)
+# library(reshape2)
+
+# copy the following so you can do sme()
+WORKDIRECTORY='/work/directory'
+THISFILE     ='thisfile.r'
+setwd(WORKDIRECTORY)
+sme <- function()
+{
+    setwd(WORKDIRECTORY)
+    source(THISFILE)
+}
+
+explore.FSJ386323 <- function()
+{
+    transfer <- function()
+    {
+    }
+    load <- function()
+    {
+        datatext = """
+        order               time
+        h()=random()        18.921395
+        h()=offset          8.161882
+        h()=-offset         74.934069
+
+    }
+
+    clean <- function(d)
+    {
+    }
+
+    func <- function(d)
+    {
+    }
+
+    do_main <- function()
+    {
+        d = load()
+        d = clean(d)
+        func(d)
+    }
+    do_main()
+}
+
+main <- function()
+{
+    explore.FSJ386323()
+}
+main()
